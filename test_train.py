7068b326007f26c53b7b1dc2bd614f67f3a18bcc
Djondang <joulebecobusi@gmail.com>
2023-12-13 16:43:09 +0100

    Unitary tests

---
+++ test_train.py
@@ -0,0 +1,58 @@
+import unittest
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from your_module_name import load_data, preprocess_data, split_data, scale_data, train_model, evaluate_model
+
+class TestTrain(unittest.TestCase):
+
+    def setUp(self):
+        # Charger des données de test
+        self.test_data = pd.DataFrame({
+            'Pclass': [1, 2, 3, 1, 2],
+            'Sex': ['male', 'female', 'male', 'female', 'male'],
+            'Age': [30, 25, 35, 28, 40],
+            'Survived': [1, 0, 1, 0, 1]
+        })
+
+    def test_load_data(self):
+        # Testez si la fonction load_data charge correctement les données
+        df = load_data(self.test_data)
+        self.assertTrue(isinstance(df, pd.DataFrame))
+
+    def test_preprocess_data(self):
+        # Testez si la fonction preprocess_data renvoie les features (X) et les étiquettes (y) correctement
+        X, y = preprocess_data(self.test_data)
+        self.assertTrue(isinstance(X, pd.DataFrame))
+        self.assertTrue(isinstance(y, pd.Series))
+
+    def test_split_data(self):
+        # Testez si la fonction split_data divise correctement les données en ensembles d'entraînement et de test
+        X_train, X_test, y_train, y_test = split_data(self.test_data)
+        self.assertTrue(isinstance(X_train, pd.DataFrame))
+        self.assertTrue(isinstance(X_test, pd.DataFrame))
+        self.assertTrue(isinstance(y_train, pd.Series))
+        self.assertTrue(isinstance(y_test, pd.Series))
+
+    def test_scale_data(self):
+        # Testez si la fonction scale_data normalise correctement les données
+        X_train, X_test = scale_data(self.test_data, self.test_data)
+        self.assertTrue(isinstance(X_train, pd.DataFrame))
+        self.assertTrue(isinstance(X_test, pd.DataFrame))
+
+    def test_train_model(self):
+        # Testez si la fonction train_model entraîne correctement le modèle
+        model = DummyModel()  # Remplacez DummyModel par le modèle que vous utilisez
+        X_train, _, y_train, _ = split_data(self.test_data)
+        train_model(model, X_train, y_train)
+        # Ajoutez des assertions pour vérifier que le modèle a été correctement entraîné
+
+    def test_evaluate_model(self):
+        # Testez si la fonction evaluate_model évalue correctement le modèle
+        model = DummyModel()  # Remplacez DummyModel par le modèle que vous utilisez
+        X_train, X_test, y_train, y_test = split_data(self.test_data)
+        train_model(model, X_train, y_train)
+        results = evaluate_model(model, X_test, y_test)
+        # Ajoutez des assertions pour vérifier les résultats
+
+if __name__ == '__main__':
+    unittest.main()
--- train.py
+++ train.py
@@ -1,79 +1,96 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, classification_report, precision_score, recall_score, f1_score, confusion_matrix
+from sklearn.metrics import (
+    accuracy_score,
+    classification_report,
+    precision_score,
+    recall_score,
+    f1_score,
+    confusion_matrix,
+)
 
-def load_data(file_path):
-    return pd.read_csv(file_path, sep = ',')
 
-def preprocess_data(data):
-    X = data[['Pclass', 'Sex', 'Age']]
-    X = pd.get_dummies(X, columns=['Sex'], drop_first=True)
-    y = data['Survived']
-    return X, y
+class Train:
+    def load_data(self, file_path):
+        """Load data from a CSV file."""
+        return pd.read_csv(file_path, sep=',')
 
-def split_data(X, y, test_size=0.2, random_state=42):
-    return train_test_split(X, y, test_size=test_size, random_state=random_state)
+    def preprocess_data(self, data):
+        """Preprocess data and return features (X) and labels (y)."""
+        X = data[['Pclass', 'Sex', 'Age']]
+        X = pd.get_dummies(X, columns=['Sex'], drop_first=True)
+        y = data['Survived']
+        return X, y
 
-def scale_data(X_train, X_test):
-    scaler = StandardScaler()
-    X_train_scaled = scaler.fit_transform(X_train)
-    X_test_scaled = scaler.transform(X_test)
-    return X_train_scaled, X_test_scaled
+    def split_data(self, X, y, test_size=0.2, random_state=42):
+        """Split data into training and testing sets."""
+        return train_test_split(X, y, test_size=test_size, random_state=random_state)
 
-def train_model(model, X_train, y_train):
-    model.fit(X_train, y_train)
+    def scale_data(self, X_train, X_test):
+        """Scale data using StandardScaler."""
+        scaler = StandardScaler()
+        X_train_scaled = scaler.fit_transform(X_train)
+        X_test_scaled = scaler.transform(X_test)
+        return X_train_scaled, X_test_scaled
 
-def evaluate_model(model, X_test, y_test):
-    y_pred = model.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
-    report = classification_report(y_test, y_pred)
-    precision = precision_score(y_test, y_pred)
-    recall = recall_score(y_test, y_pred)
-    f1 = f1_score(y_test, y_pred)
-    confusion = confusion_matrix(y_test, y_pred)
-    return accuracy, report, precision, recall, f1, confusion
+    def train_model(self, model, X_train, y_train):
+        """Train the specified model."""
+        model.fit(X_train, y_train)
 
-def main():
-    # Charger les données
-    train_data = load_data('data/train.csv')
+    def evaluate_model(self, model, X_test, y_test):
+        """Evaluate the trained model and return metrics."""
+        y_pred = model.predict(X_test)
+        accuracy = accuracy_score(y_test, y_pred)
+        report = classification_report(y_test, y_pred)
+        precision = precision_score(y_test, y_pred)
+        recall = recall_score(y_test, y_pred)
+        f1 = f1_score(y_test, y_pred)
+        confusion = confusion_matrix(y_test, y_pred)
+        return accuracy, report, precision, recall, f1, confusion
 
-    # Prétraiter les données
-    X, y = preprocess_data(train_data)
+    def main(self):
+        # Charger les données
+        train_data = self.load_data('data/train.csv')
 
-    # Diviser les données en ensembles d'entraînement et de test
-    X_train, X_test, y_train, y_test = split_data(X, y)
+        # Prétraiter les données
+        X, y = self.preprocess_data(train_data)
 
-    # Normaliser les données si nécessaire
-    X_train_scaled, X_test_scaled = scale_data(X_train, X_test)
+        # Diviser les données en ensembles d'entraînement et de test
+        X_train, X_test, y_train, y_test = self.split_data(X, y)
 
-    # Entraîner des modèles
-    model_lr = LogisticRegression()
-    model_dt = DecisionTreeClassifier()
-    model_rf = RandomForestClassifier()
+        # Normaliser les données si nécessaire
+        X_train_scaled, X_test_scaled = self.scale_data(X_train, X_test)
 
-    train_model(model_lr, X_train_scaled, y_train)
-    train_model(model_dt, X_train_scaled, y_train)
-    train_model(model_rf, X_train_scaled, y_train)
+        # Entraîner des modèles
+        model_lr = LogisticRegression()
+        model_dt = DecisionTreeClassifier()
+        model_rf = RandomForestClassifier()
 
-    # Évaluer les modèles
-    results_lr = evaluate_model(model_lr, X_test_scaled, y_test)
-    results_dt = evaluate_model(model_dt, X_test_scaled, y_test)
-    results_rf = evaluate_model(model_rf, X_test_scaled, y_test)
+        self.train_model(model_lr, X_train_scaled, y_train)
+        self.train_model(model_dt, X_train_scaled, y_train)
+        self.train_model(model_rf, X_train_scaled, y_train)
 
-    # Afficher les résultats
-    print("Logistic Regression Results:")
-    print("Accuracy:", results_lr[0])
-    print("Classification Report:\n", results_lr[1])
-    print("Precision:", results_lr[2])
-    print("Recall:", results_lr[3])
-    print("F1-score:", results_lr[4])
-    print("Confusion Matrix:\n", results_lr[5])
+        # Évaluer les modèles
+        results_lr = self.evaluate_model(model_lr, X_test_scaled, y_test)
+        results_dt = self.evaluate_model(model_dt, X_test_scaled, y_test)
+        results_rf = self.evaluate_model(model_rf, X_test_scaled, y_test)
 
-    # Répéter pour les autres modèles...
+        # Afficher les résultats (à adapter selon vos besoins)
+        print("Logistic Regression Results:")
+        print("Accuracy:", results_lr[0])
+        print("Classification Report:\n", results_lr[1])
+        print("Precision:", results_lr[2])
+        print("Recall:", results_lr[3])
+        print("F1-score:", results_lr[4])
+        print("Confusion Matrix:\n", results_lr[5])
+
+        # Répéter pour les autres modèles...
+
 
 if __name__ == "__main__":
-    main()
+    model_instance = Train()
+    model_instance.main()
