04cd700a9d6b5ef3099dc85914170f4a50f7b3bd
Czmil Dossou-yovo <90108367+CzmilDos@users.noreply.github.com>
2023-12-13 16:35:59 +0100

    Update test_process.py

--- test_process.py
+++ test_process.py
@@ -32,4 +32,5 @@ def test_data_cleaning():
     assert cleaned_data['age'].dtype == int
 
     # Vérification de l'arrondissement du prix du billet à deux chiffres après la virgule
-    assert (cleaned_data['fare'] % 0.01 == 0).all()
+    assert all(abs(cleaned_data['fare'] % 0.01) < 1e-10)
+
