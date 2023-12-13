bac9c8cf55ac39c54b30aca5ab009dd9abc8b3d9
Djondang <joulebecobusi@gmail.com>
2023-12-13 16:55:48 +0100

    Unitry Test2

--- test_train.py
+++ test_train.py
@@ -1,7 +1,6 @@
 import unittest
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from your_module_name import load_data, preprocess_data, split_data, scale_data, train_model, evaluate_model
+from your_module_name import Train
 
 class TestTrain(unittest.TestCase):
 
@@ -16,18 +15,18 @@ class TestTrain(unittest.TestCase):
 
     def test_load_data(self):
         # Testez si la fonction load_data charge correctement les données
-        df = load_data(self.test_data)
+        df = Train().load_data(self.test_data)
         self.assertTrue(isinstance(df, pd.DataFrame))
 
     def test_preprocess_data(self):
         # Testez si la fonction preprocess_data renvoie les features (X) et les étiquettes (y) correctement
-        X, y = preprocess_data(self.test_data)
+        X, y = Train().preprocess_data(self.test_data)
         self.assertTrue(isinstance(X, pd.DataFrame))
         self.assertTrue(isinstance(y, pd.Series))
 
     def test_split_data(self):
         # Testez si la fonction split_data divise correctement les données en ensembles d'entraînement et de test
-        X_train, X_test, y_train, y_test = split_data(self.test_data)
+        X_train, X_test, y_train, y_test = Train().split_data(self.test_data)
         self.assertTrue(isinstance(X_train, pd.DataFrame))
         self.assertTrue(isinstance(X_test, pd.DataFrame))
         self.assertTrue(isinstance(y_train, pd.Series))
@@ -35,23 +34,19 @@ class TestTrain(unittest.TestCase):
 
     def test_scale_data(self):
         # Testez si la fonction scale_data normalise correctement les données
-        X_train, X_test = scale_data(self.test_data, self.test_data)
+        X_train, X_test = Train().scale_data(self.test_data, self.test_data)
         self.assertTrue(isinstance(X_train, pd.DataFrame))
         self.assertTrue(isinstance(X_test, pd.DataFrame))
 
     def test_train_model(self):
-        # Testez si la fonction train_model entraîne correctement le modèle
-        model = DummyModel()  # Remplacez DummyModel par le modèle que vous utilisez
-        X_train, _, y_train, _ = split_data(self.test_data)
-        train_model(model, X_train, y_train)
+        # Testez si la fonction train_model entraîne correctement le modèle LogisticRegression
+        model = Train().train_model(LogisticRegression(), self.test_data, self.test_data['Survived'])
         # Ajoutez des assertions pour vérifier que le modèle a été correctement entraîné
 
     def test_evaluate_model(self):
-        # Testez si la fonction evaluate_model évalue correctement le modèle
-        model = DummyModel()  # Remplacez DummyModel par le modèle que vous utilisez
-        X_train, X_test, y_train, y_test = split_data(self.test_data)
-        train_model(model, X_train, y_train)
-        results = evaluate_model(model, X_test, y_test)
+        # Testez si la fonction evaluate_model évalue correctement le modèle LogisticRegression
+        model = Train().train_model(LogisticRegression(), self.test_data, self.test_data['Survived'])
+        results = Train().evaluate_model(model, self.test_data, self.test_data['Survived'])
         # Ajoutez des assertions pour vérifier les résultats
 
 if __name__ == '__main__':
